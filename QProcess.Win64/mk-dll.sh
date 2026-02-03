d745b3fd89ab65ed49536b3d71d24192a16976ba
JavaCommons Technologies <javacommons@gmail.com>
2026-02-03 21:31:54 +0900

    .

--- QProcess.Win64/mk-dll.sh
+++ QProcess.Win64/mk-dll.sh
@@ -7,4 +7,4 @@ swig -csharp -c++ -cppext cpp -outdir . QProcess_Win64.i
 dll-gen QProcess_Win64.cpp
 pro64 -s QProcess_Win64.pro
 cp -p QProcess_Win64.mingw64.static.dll QProcess_Win64.dll
-cp -p QProcess_Win64.dll ~/cmd/
+#cp -p QProcess_Win64.dll ~/cmd/
