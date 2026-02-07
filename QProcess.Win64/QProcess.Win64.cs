edceecf57a9c166e99d72c4cbf058cc10969d191
JavaCommons Technologies <javacommons@gmail.com>
2026-02-07 13:35:23 +0900

    QProcess.Win64-v2026.0207.1335.15

--- QProcess.Win64/QProcess.Win64.cs
+++ QProcess.Win64/QProcess.Win64.cs
@@ -4,17 +4,24 @@ namespace QProcess;
 
 public class Win64
 {
-    public static int Execute(string exe, string[] args, string cwd = null)
+    public static int Execute(string exe, string[] args, string? cwd = null)
     {
         if (cwd == null) cwd = "";
         Global.EasyObject.Log(new { exe = exe, args = args, cwd = cwd });
         return QProcess_Win64.Execute3(exe, string.Join("\t", args), cwd);
     }
-    public static void Launch(string exe, string[] args, string cwd = null)
+    public static void Launch(string exe, string[] args, string? cwd = null)
     {
         if (cwd == null) cwd = "";
         Global.EasyObject.Log(new { exe = exe, args = args, cwd = cwd });
         QProcess_Win64.Launch3(exe, string.Join("\t", args), cwd);
         //QProcess_Win64.Execute4(true, exe, string.Join("\t", args), cwd);
+    }
+    public static int Run(bool windowed, string exe, string[] args, string? cwd = null)
+    {
+        if (cwd == null) cwd = "";
+        Global.EasyObject.Log(new { exe = exe, args = args, cwd = cwd });
+        int exitCode = QProcess_Win64.Execute4(windowed, exe, string.Join("\t", args), cwd);
+        return exitCode;
     }
 }
